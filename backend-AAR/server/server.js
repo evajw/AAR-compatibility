162ea0a278dc41195ec4a3e21da93a3d65c25107
Eva <ejwesterhout@hotmail.com>
2025-12-17 16:48:23 +0100

    changes to the backend, I am trying to get the values of database into the browser, I still need to check out how ejs (dynamic) and html (static) files precisly work and how to make a nice overview of the server

--- backend-AAR/server/server.js
+++ backend-AAR/server/server.js
@@ -6,7 +6,6 @@ const path = require("path");
 const { Pool } = pkg;
 const PORT = 3000;
 
-
 const pool = new Pool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -14,6 +13,9 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     port: process.env.DB_PORT
 });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //basic route
 app.get("/hello", (req, res) => {
@@ -54,6 +56,89 @@ app.get("/api/tankers", async (req, res) => {
         res.status(500).send("Database error")
 }
 });
+
+app.set("view engine", "ejs")
+
+app.post("/submit", (req, res) => {
+    const {
+        input1: tankerNation,
+        input2: tankerType,
+        input3: tankerModel,
+        input4: receiverNation,
+        input5: receiverType,
+        input6: receiverModel
+    } = req.body;
+
+    try {
+        const result = await pool.query = (`
+            SELECT result FROM compatibility
+            WHERE tanker_nation = $1
+            AND tanker_type = $2
+            AND tanker_model =$3
+            AND receiver_nation =$4
+            AND receiver_type = $5
+            AND receiver_model = $6`,
+            [tankerNation, tankerType, tankerModel, receiverNation, receiverType, receiverModel]
+        );
+
+        if (result.rows.length>0) {
+            res.render("compatibility", {
+                found: true,
+                tankerNation,
+                tankerType,
+                tankerModel,
+                receiverNation,
+                receiverType,
+                receiverModel,
+                result: result.rows[0].result
+            });
+        } else {
+            res.render("compatibility", {
+                found: false,
+                tankerNation,
+                tankerType,
+                tankerModel,
+                receiverNation,
+                receiverType,
+                receiverModel,
+                message: "No compatibility data found"
+            });
+        }
+
+    } catch (err) {
+            console.error(err);
+            return res.status(500).render("result", {
+                found: false,
+                message: "Database error"
+            });
+    }    
+});
+
+// app.get("/", (req, res) => {
+//     db.serialize(() => {
+//         db.all("SELECT DISTINCT tanker_nation FROM compatibility", (e1, tanker) => {
+//             db.all("SELECT DISTINCT tanker_type FROM compatibility", (e2, tankerType) => {
+//                 db.all("SELECT DISTINCT tanker_model FROM compatibility", (e3, tankerModel) => {
+//                     db.all("SELECT DISTINCT receiver_nation FROM compatibility", (e4, receiver) => {
+//                         db.all("SELECT DISTINCT receiver_type FROM compatibility", (e5, receiverType) => {
+//                             db.all("SELECT DISTINCT receiver_model FROM compatiblity", (e6, receiverModel))
+
+//                             res.render("index", {
+//                                 tanker,
+//                                 tankerType,
+//                                 tankerModel,
+//                                 receiver,
+//                                 receiverType,
+//                                 receiverModel
+//                             });
+//                         });     
+//                     });
+//                 });
+//             });
+//         });
+//     });
+// });
+
 
 
 //Start server
