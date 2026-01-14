41f6b65e789c39b4fbc9894123837aca83ce41b5
Fabian <22062122@student.hhs.nl>
2026-01-14 13:17:15 +0100

    Mijn wijzigingen verplaatst vanuit main

---
+++ frontend-AAR/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import PictureAAR from './Pictures/Picture AAR.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>AAR</h1>
+      <img src={PictureAAR}/>
+    </div>
+  )
+}
+
+export default App
